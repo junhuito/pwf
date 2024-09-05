68a6a4ea5b7bc1469e6a8587881141e94fb37280
jun <junhuito5771@gmail.com>
2024-09-05 16:31:15 +0800

    test

---
+++ .next/static/VmqnYxY-q5hJegT2-wHdm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/273-a88b0d89a0322277.js","static/css/628765f20b848f76.css","static/chunks/pages/index-517e22ee9cdeb95f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
